270f693d2c2b97d50602afd2db3efad1fed6010a
Xaxurro <xaxo.victoriano@gmail.com>
2024-10-29 23:33:33 -0300

    feat:modifiable entries

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sed -i '/^### DATA ###$/q' wofi-emoji
 
+CONFIG_FILE=~/.config/wofi-emoji.rc
 curl https://raw.githubusercontent.com/muan/emojilib/v3.0.12/dist/emoji-en-US.json \
   | jq --raw-output '. | to_entries | .[] | .key + " " + (.value | join(" ") | sub("_"; " "; "g"))' \
-  >> wofi-emoji
+  > $CONFIG_FILE
